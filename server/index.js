30aea686cfa84028a21f33540319b8dd376fd770
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-06 10:31:30 +0200

    connection à mongodb

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// ! Connecter mongoose à notre data base mongodb
+mongoose.connect('mongodb://localhost/jwt_auth',{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}, () => {
+    console.log('Connected to MongoDB')
+});
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello there');
+})
+
+app.listen(8000);
