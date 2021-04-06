d2194cc25b23664fe9b07ce6fda7ae3ad53af6e5
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-06 10:40:21 +0200

    Creation du dossier model pour les user

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// ! Connecter mongoose à notre data base mongodb
+// ! Connecter mongoose à notre data base mongodb dans un model séparé
 mongoose.connect('mongodb://localhost/jwt_auth',{
     useNewUrlParser : true,
     useUnifiedTopology : true
 }, () => {
     console.log('Connected to MongoDB')
 });
+
 const app = express();
 
 app.get('/', (req,res) => {
