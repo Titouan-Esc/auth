b32663d74f9c78791f2912bc3717c88beb4a57af
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-08 09:57:32 +0200

    Instalation des routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom'
+
 import './App.css';
 import NavBar from './components/NavBar';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import User from './pages/User';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <main className="form-signin">
-        <Login/>
+        <BrowserRouter>
+          <Route path = "/" exact component={Home}/>
+          <Route path = "/register"  component={Register}/>
+          <Route path = "/login"  component={Login}/>
+          <Route path = "/user"  component={User}/>
+        </BrowserRouter>
       </main>
     </div>
   );
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,14 +2,10 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <a className="navbar-brand" href="www.google.com">JWT-Auth</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-
             <div className="collapse navbar-collapse" id="navbarsExample04">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                <a className="nav-link" href="www.google.com">Register <span class="sr-only">(current)</span></a>
+                <a className="nav-link" href="www.google.com">Register</a>
                 </li>
                 <li className="nav-item">
                 <a className="nav-link" href="www.google.com">Login</a>
