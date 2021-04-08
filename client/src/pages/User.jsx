1f897dd95d98f264eb9e99fbfebbf6fe219a4aa3
Mes Morts <titouanescorneboueu@gmail.com>
2021-04-08 12:31:43 +0200

    Ajout du fetch logout

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+
 
 
 const User = () => {
@@ -23,10 +25,38 @@ const User = () => {
         )()
     }, []);
 
+    const logout = async () => {
+        await fetch('http://localhost:8000/api/user/logout', {
+            // ? la methode et une methode POST
+            method : 'POST',
+            // ? 
+            headers : {'Content-Type' : 'application/json'},
+            // ? Récupérer les cookies que nous avons créer
+            credentials : 'include',
+        });
+    }
+
+    // ? Créer une variable let link
+    let link;
+
+    // ? Faire une condition pour dire que si username n'est pas, envoyer vers login et si oui logout. Cette condition est du JSX donc du HTML avec un peu de JS
+    if(!username){
+        link = (
+            <Link to='/login' className='btn btn-success'>Login</Link>
+        )
+    }else{
+        link = (
+            <button onClick={logout} className='btn btn-danger'>Logout</button>
+        )
+    }
+
+
+
     return (
         <div>
             {/* Reprend le username du client qui c'est connecté */}
             {username ? `Vous êtes connecté ${username}` : 'Se connecter'}
+            {link}
         </div>
     )
 }
